8482542a8f8aca0d103d5ba3f3bc1c22a7a355b4
dev-sundaysolar <dev.sundaysolar@gmail.com>
2024-12-22 22:10:58 +0330

    send email refactor

--- src/api/contact/content-types/contact/lifecycles.ts
+++ src/api/contact/content-types/contact/lifecycles.ts
@@ -4,21 +4,23 @@ export default {
   async afterCreate(event) {
     const { result } = event;
 
-    try {
-      await strapi.plugins["email"].services.email.send({
-        to: env("DEFAULT_TO_EMAIL"),
-        from: env("DEFAULT_FROM_EMAIL"),
-        subject: `New Contact: ${result.email}`,
-        text: `email: ${result.email} message: ${result.content}`,
-      });
-      await strapi.plugins["email"].services.email.send({
-        to: result.email,
-        from: env("DEFAULT_FROM_EMAIL"),
-        subject: `Sunday Solar Support`,
-        text: `We received your contact form`,
-      });
-    } catch (err) {
-      console.log(err);
+    if (result?.email && result?.content) {
+      try {
+        await strapi.plugins["email"].services.email.send({
+          to: env("DEFAULT_TO_EMAIL"),
+          from: env("DEFAULT_FROM_EMAIL"),
+          subject: `New Contact: ${result.email}`,
+          text: `email: ${result.email} message: ${result.content}`,
+        });
+        await strapi.plugins["email"].services.email.send({
+          to: result.email,
+          from: env("DEFAULT_FROM_EMAIL"),
+          subject: `Sunday Solar Support`,
+          text: `We received your contact form`,
+        });
+      } catch (err) {
+        console.log(err);
+      }
     }
   },
 };
