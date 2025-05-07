682a9d3b2713bb826399c481625f8ea3fe61c55f
dev-sundaysolar <dev.sundaysolar@gmail.com>
2025-05-08 00:18:14 +0300

    - Added totalReviews field to the review component - Required stars field - Renamed uiType to heroUI - Renamed normal heroUI to full-width - Populated google review in contact configurator page

--- src/api/contact-configurator-page/controllers/contact-configurator-page.ts
+++ src/api/contact-configurator-page/controllers/contact-configurator-page.ts
@@ -2,6 +2,27 @@
  * contact-configurator-page controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
+import { populateCollection } from "../../../utils/populateCollection";
 
-export default factories.createCoreController('api::contact-configurator-page.contact-configurator-page');
+export default factories.createCoreController(
+  "api::contact-configurator-page.contact-configurator-page",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+
+      const entity = await strapi
+        .documents("api::contact-configurator-page.contact-configurator-page")
+        .findFirst(sanitizedQueryParams);
+
+      return populateCollection(
+        strapi,
+        "api::contact-configurator-page.contact-configurator-page",
+        "contact-configurator-page",
+        "showContactConfiguratorPage",
+        entity,
+        sanitizedQueryParams
+      );
+    },
+  })
+);
